95477179f3a67edeb7e1a0eedcccc9ccbf6f3e96
Kevin Guzmán <guzmankevin420@gmail.com>
2025-09-23 12:27:52 -0500

    some changes

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def startup_event():
 @app.get("/")
 async def root():
     """Endpoint raíz."""
-    return {"message": "Estoy molestoOooOO."}
+    return {"message": "Perdoname Minchito :("}
 
 if __name__ == "__main__":
     import uvicorn
