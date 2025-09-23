f5ced22021a6324b20fef10c60b0b06fa31ba4cd
Kevin Guzmán <guzmankevin420@gmail.com>
2025-09-23 09:42:07 -0500

    deploy.yml updated

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def startup_event():
 @app.get("/")
 async def root():
     """Endpoint raíz."""
-    return {"message": "Trying to deploy with shadowlight hiroi sekai"}
+    return {"message": "Masha se asustó de jugar"}
 
 if __name__ == "__main__":
     import uvicorn
